0c2c9e83529ab236c3ceec62abe8d6fdfdca86c3
Nilo Soares <nilosoares@hotmail.com>
2022-03-03 22:23:53 -0300

    Add the "natural_implode" function

---
+++ tests/ArrayFunctions/NaturalImplodeTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace CancioLabs\Functions\Tests\ArrayFunctions;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use function CancioLabs\Functions\natural_implode;
+
+class NaturalImplodeTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function shouldReturnEmptyStringWhenArrayIsEmpty(): void
+    {
+        natural_implode([]);
+        $this->expectException(InvalidArgumentException::class);
+
+        natural_implode([null]);
+        $this->expectException(InvalidArgumentException::class);
+
+        natural_implode(['']);
+        $this->expectException(InvalidArgumentException::class);
+
+        natural_implode([1]);
+        $this->expectException(InvalidArgumentException::class);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnElementValueWhenArrayHasOneElement(): void
+    {
+        $str = natural_implode(['1']);
+        $this->assertSame('1', $str);
+
+        $str = natural_implode(['foo']);
+        $this->assertSame('foo', $str);
+
+        $str = natural_implode([1 => 'bar']);
+        $this->assertSame('bar', $str);
+
+        $str = natural_implode(['foo' => 'zoo']);
+        $this->assertSame('zoo', $str);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldImplodeStringWhenArrayHasMultipleElements(): void
+    {
+        $str = natural_implode(['foo', 'bar']);
+        $this->assertSame('foo and bar', $str);
+
+        $str = natural_implode([1 => 'foo', 2 => 'bar', 3 => 'zoo']);
+        $this->assertSame('foo, bar and zoo', $str);
+
+        $str = natural_implode(['foo' => '1', 'bar' => '2', 'zoo' => '3']);
+        $this->assertSame('1, 2 and 3', $str);
+
+        $str = natural_implode(['apple', 'banana', 'cashew'], ', ', ' or ');
+        $this->assertSame('apple, banana or cashew', $str);
+
+        $str = natural_implode(['Rio', 'New York', 'Jerusalem', 'Sydney'], '... ', '... and ');
+        $this->assertSame('Rio... New York... Jerusalem... and Sydney', $str);
+    }
+
+}
