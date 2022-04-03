491dec458b9eb48bdd3e9a086723a60988526819
Nilo Soares <nilosoares@hotmail.com>
2022-04-03 08:34:25 -0300

    Add the "year_to_second" function

---
+++ src/ConverterFunctions/Time/year_to_second.func.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CancioLabs\Functions\ConverterFunctions\Time;
+
+use Webmozart\Assert\Assert;
+
+if (!function_exists('year_to_second')) {
+    /**
+     * Converts Years into Seconds.
+     *
+     * @param int|float $years
+     * @return int
+     */
+    function year_to_second($years)
+    {
+        Assert::numeric($years);
+
+        return $years * 31536000;
+    }
+}
